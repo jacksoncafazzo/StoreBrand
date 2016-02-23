d977785cf717cb04ab737a5bccdbb021610385e3
Chris Kuiper and Labrynth <chris.kuiper@gmail.com and student@epicodus.net>
2016-02-23 11:53:39 -0800

    add tests for saving to the datebase, assigning an id to the new item and test to find a new item in the database

--- Tests/CategoryTest.cs
+++ Tests/CategoryTest.cs
@@ -34,6 +34,51 @@ namespace ToDoList
       Assert.Equal(firstCategory, secondCategory);
     }
 
+    [Fact]
+    public void Test_Save_SavesCategoryToDatabase()
+    {
+       //Arrange
+       Category testCategory = new Category("Household chores");
+       testCategory.Save();
+
+       //Act
+       List<Category> result = Category.GetAll();
+       List<Category> testList = new List<Category>{testCategory};
+
+       //Assert
+       Assert.Equal(testList, result);
+    }
+
+    [Fact]
+    public void Test_Save_AssignsIdToCategoryObject()
+    {
+      //Arrange
+      Category testCategory = new Category("Household chores");
+      testCategory.Save();
+
+      //Act
+      Category savedCategory = Category.GetAll()[0];
+
+      int result = savedCategory.GetId();
+      int testId = testCategory.GetId();
+
+      //Assert
+      Assert.Equal(testId, result);
+    }
+
+    [Fact]
+    public void Test_Find_FindsCategoryInDatabase()
+    {
+      //Arrange
+      Category testCategory = new Category("Household chores");
+      testCategory.Save();
+
+      //Act
+      Category foundCategory = Category.Find(testCategory.GetId());
+
+      //Assert
+      Assert.Equal(testCategory, foundCategory);
+    }
 
     public void Dispose()
     {
